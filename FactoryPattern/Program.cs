80cb34e626c7f49f6f20a6270c01cb94a4967808
Thomas Marble <tmarble3@gmail.com>
2022-03-25 16:30:47 -0600

    initial commit

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter if the vehicle has a bed");
+            bool bed;
+            var input = bool.TryParse(Console.ReadLine(), out bed);
+
+            var vehicle = VehicleFactory.GetVehicle(bed);
+            vehicle.Drive();
+
+
+
+        }
+
+    }
+}
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(bool bed)
+        {
+            switch(bed)
+            {
+                case true:
+                    return new Truck();
+                case false:
+                    return new Car();
+                
+            }
+
+        }
+    }
+}
